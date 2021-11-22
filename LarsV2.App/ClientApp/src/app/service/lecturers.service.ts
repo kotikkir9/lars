137ae5705cd6ac466095dec5bf81b01f3c6fcb50
Christian <christian@skjern-net.dk>
2021-11-22 10:00:33 +0100

    Tilføjer test i LecturersService

--- LarsV2.App/ClientApp/src/app/service/lecturers.service.ts
+++ LarsV2.App/ClientApp/src/app/service/lecturers.service.ts
@@ -14,6 +14,137 @@ export interface iLecturersServiceData {
   records: iLecturers[];
 }
 
+const testData: iLecturersServiceData = {
+  metadata: {
+    totalCount: 10,
+    pageSize: 10,
+    currentPage: 1,
+    totalPages: 2
+  },
+  records: [
+    {
+      id: 1,
+      firstName: "Jan Pan",
+      lastName: "Nees",
+      email: "jan@eamv.dk",
+      phoneNumber: "+45 12345678",
+      cvPath: null,
+      imagePath: null,
+      isExternal: false,
+      knowledge: null,
+      lecturerSubjects: null
+    },
+    {
+      id: 2,
+      firstName: "James",
+      lastName: "Hetfield",
+      email: "jameshetfield@metallica.com",
+      phoneNumber: "+45 69696969",
+      cvPath: null,
+      imagePath: null,
+      isExternal: false,
+      knowledge: null,
+      lecturerSubjects: null
+    },
+    {
+      id: 3,
+      firstName: "Flemming",
+      lastName: "Efternavn",
+      email: "flemming@eamv.dk",
+      phoneNumber: "+45 11111111",
+      cvPath: null,
+      imagePath: null,
+      isExternal: false,
+      knowledge: null,
+      lecturerSubjects: null
+    },
+    {
+      id: 4,
+      firstName: "Till",
+      lastName: "Lindemann",
+      email: "lindemann@eamv.dk",
+      phoneNumber: "+45 98765432",
+      cvPath: null,
+      imagePath: null,
+      isExternal: false,
+      knowledge: null,
+      lecturerSubjects: null
+    },
+    {
+      id: 5,
+      firstName: "Bob",
+      lastName: "Ross",
+      email: "bob@eamv.dk",
+      phoneNumber: "+45 44444444",
+      cvPath: null,
+      imagePath: null,
+      isExternal: false,
+      knowledge: null,
+      lecturerSubjects: null
+    },
+    {
+      id: 6,
+      firstName: "Vladimir",
+      lastName: "Putin",
+      email: "putin@eamv.dk",
+      phoneNumber: "+45 55555555",
+      cvPath: null,
+      imagePath: null,
+      isExternal: false,
+      knowledge: null,
+      lecturerSubjects: null
+    },
+    {
+      id: 7,
+      firstName: "Donald",
+      lastName: "Trump",
+      email: "trump@eamv.dk",
+      phoneNumber: "+45 66666666",
+      cvPath: null,
+      imagePath: null,
+      isExternal: false,
+      knowledge: null,
+      lecturerSubjects: null
+    },
+    {
+      id: 8,
+      firstName: "Joe",
+      lastName: "Rogan",
+      email: "joe@eamv.dk",
+      phoneNumber: "+45 77777777",
+      cvPath: null,
+      imagePath: null,
+      isExternal: false,
+      knowledge: null,
+      lecturerSubjects: null
+    },
+    {
+      id: 9,
+      firstName: "Max",
+      lastName: "Verstappen",
+      email: "max@eamv.dk",
+      phoneNumber: "+45 88888888",
+      cvPath: null,
+      imagePath: null,
+      isExternal: false,
+      knowledge: null,
+      lecturerSubjects: null
+    },
+    {
+      id: 10,
+      firstName: "Lionel",
+      lastName: "Messi",
+      email: "messi@eamv.dk",
+      phoneNumber: "+45 99999999",
+      cvPath: null,
+      imagePath: null,
+      isExternal: false,
+      knowledge: null,
+      lecturerSubjects: null
+    }
+  ]
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -22,136 +153,14 @@ export class LecturersService {
   constructor(private http: HttpClient) { }
 
   getData(pageSize: number, pageIndex: number): iLecturersServiceData {
+    let buildData: iLecturersServiceData = {
+      metadata: testData.metadata,
+      records: []
+    };
+
+    buildData.records = testData.records.slice(pageSize * pageIndex, Math.min(testData.records.length , (pageSize * pageIndex + pageSize)));
+
     console.log(`pageSize: ${pageSize}, pageIndex: ${pageIndex}`);
-    return {
-      metadata: {
-        totalCount: 13,
-        pageSize: 10,
-        currentPage: 1,
-        totalPages: 2
-      },
-      records: [
-        {
-          id: 1,
-          firstName: "Jan Pan",
-          lastName: "Nees",
-          email: "jan@eamv.dk",
-          phoneNumber: "+45 12345678",
-          cvPath: null,
-          imagePath: null,
-          isExternal: false,
-          knowledge: null,
-          lecturerSubjects: null
-        },
-        {
-          id: 2,
-          firstName: "James",
-          lastName: "Hetfield",
-          email: "jameshetfield@metallica.com",
-          phoneNumber: "+45 69696969",
-          cvPath: null,
-          imagePath: null,
-          isExternal: false,
-          knowledge: null,
-          lecturerSubjects: null
-        },
-        {
-          id: 3,
-          firstName: "Flemming",
-          lastName: "Efternavn",
-          email: "flemming@eamv.dk",
-          phoneNumber: "+45 11111111",
-          cvPath: null,
-          imagePath: null,
-          isExternal: false,
-          knowledge: null,
-          lecturerSubjects: null
-        },
-        {
-          id: 4,
-          firstName: "Till",
-          lastName: "Lindemann",
-          email: "lindemann@eamv.dk",
-          phoneNumber: "+45 98765432",
-          cvPath: null,
-          imagePath: null,
-          isExternal: false,
-          knowledge: null,
-          lecturerSubjects: null
-        },
-        {
-          id: 5,
-          firstName: "Bob",
-          lastName: "Ross",
-          email: "bob@eamv.dk",
-          phoneNumber: "+45 44444444",
-          cvPath: null,
-          imagePath: null,
-          isExternal: false,
-          knowledge: null,
-          lecturerSubjects: null
-        },
-        {
-          id: 6,
-          firstName: "Vladimir",
-          lastName: "Putin",
-          email: "putin@eamv.dk",
-          phoneNumber: "+45 55555555",
-          cvPath: null,
-          imagePath: null,
-          isExternal: false,
-          knowledge: null,
-          lecturerSubjects: null
-        },
-        {
-          id: 7,
-          firstName: "Donald",
-          lastName: "Trump",
-          email: "trump@eamv.dk",
-          phoneNumber: "+45 66666666",
-          cvPath: null,
-          imagePath: null,
-          isExternal: false,
-          knowledge: null,
-          lecturerSubjects: null
-        },
-        {
-          id: 8,
-          firstName: "Joe",
-          lastName: "Rogan",
-          email: "joe@eamv.dk",
-          phoneNumber: "+45 77777777",
-          cvPath: null,
-          imagePath: null,
-          isExternal: false,
-          knowledge: null,
-          lecturerSubjects: null
-        },
-        {
-          id: 9,
-          firstName: "Max",
-          lastName: "Verstappen",
-          email: "max@eamv.dk",
-          phoneNumber: "+45 88888888",
-          cvPath: null,
-          imagePath: null,
-          isExternal: false,
-          knowledge: null,
-          lecturerSubjects: null
-        },
-        {
-          id: 10,
-          firstName: "Lionel",
-          lastName: "Messi",
-          email: "messi@eamv.dk",
-          phoneNumber: "+45 99999999",
-          cvPath: null,
-          imagePath: null,
-          isExternal: false,
-          knowledge: null,
-          lecturerSubjects: null
-        }
-      ]
-    }
+    return buildData;
   }
 }
